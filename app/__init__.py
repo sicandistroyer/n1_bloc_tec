9691fd37278d0ca639f0b5f3f9c0b8022206736c
Jaime Rodriguez <rpjosejaime@gmail.com>
2025-04-03 15:49:13 -0600

    Funcionando Hola mundo

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+from dotenv import load_dotenv 
+
+#Cargar las variables de entorno
+load_dotenv()
+
+#crear instancia
+app =  Flask(__name__)
+
+#Ruta principal home
+@app.route('/')
+def index():
+    return 'Hola mundo'
