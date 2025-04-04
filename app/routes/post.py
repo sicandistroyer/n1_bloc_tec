486c39900677851f6c2c4ab6d59b9e80d6e515aa
Jaime Rodriguez <rpjosejaime@gmail.com>
2025-04-04 16:17:58 -0600

    Insertando posts

--- app/routes/post.py
+++ app/routes/post.py
@@ -10,3 +10,20 @@ def listar_posts():
     posts = Post.query.all()
     categories = Category.query.all()
     return render_template('posts/listar_posts.html', posts=posts, categories=categories)
+
+#Ruta /post crear un nuevo post
+@posts_bp.route('/new', methods=['GET','POST'])
+def add_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        category_id = request.form.get('category_id')
+        new_post = Post(title=title, content=content, category_id=category_id)
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect(url_for('posts.listar_posts'))
+    
+    #Aqui sigue si es GET
+    categories = Category.query.all()
+    return render_template('posts/create_post.html', categories=categories)
